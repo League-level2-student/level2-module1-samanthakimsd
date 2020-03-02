4f688164b394e161738aa79b7df1c8bfcc0043db
League Student <league@iMac-20.attlocal.net>
2020-03-01 17:25:04 -0800

    i did guest book

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -2,17 +2,30 @@ package intro_to_array_lists;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	public static void main(String[] args) {
+	
 	new GuestBook().createUI();
+}
+	private void createUI() {
+		// TODO Auto-generated method stub
+		frame.add(panel);
+	panel.add(aButton);
+	panel.add(bButton);
+	frame.pack();
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton aButton = new JButton();
-	JButton bButton = new JButton();
+	JButton aButton = new JButton("ADD NAME");
+	JButton bButton = new JButton("VIEW NAME");
 	// display an input dialog that asks the user to enter a name. Add
+
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
